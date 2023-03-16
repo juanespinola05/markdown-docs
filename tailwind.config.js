e5ff357f5a16a91ec4d40689d1a22cebfdbb5aca
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-03-15 21:25:23 -0300

    Add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primaryBlue: '#27187E',
+        secondaryBlue: '#758BFD',
+        tertiaryBlue: '#AEB8FE',
+        lightGray: '#F1F2F6',
+        secondaryYellow: '#ECD444'
+      }
+    }
   },
   plugins: [],
   darkMode: 'class'
