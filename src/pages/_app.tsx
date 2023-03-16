e533d07775df7569fdadf06cfacb7f44c2ab1c64
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-03-16 14:08:52 -0300

    remove app layout from _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,13 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import { ReactElement } from 'react'
-import AppLayout from '@/layout/AppLayout'
 import { UserProvider } from '@/context/user'
 
 export default function App ({ Component, pageProps }: AppProps): ReactElement {
   return (
     <ThemeProvider attribute='class'>
       <UserProvider>
-        <AppLayout>
-          <Component {...pageProps} />
-        </AppLayout>
+        <Component {...pageProps} />
       </UserProvider>
     </ThemeProvider>
   )
