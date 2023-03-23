15ba3b4804cab2e555fd9858ec027b18049b5ffc
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-03-22 23:26:12 -0300

    create getMerdownDocByIdAndUser

--- src/lib/firebase/actions/documents.ts
+++ src/lib/firebase/actions/documents.ts
@@ -1,5 +1,5 @@
 import { MarkdownDocCreationAttributes } from '@/types'
-import { addDoc, collection, Timestamp } from 'firebase/firestore'
+import { addDoc, collection, doc, DocumentData, getDoc, Timestamp } from 'firebase/firestore'
 import { db } from '../client'
 
 const MARKDOWN_DOCS_COLLECTION_NAME = 'markdown-documents'
@@ -13,6 +13,21 @@ export const createMarkdownDoc = async (data: MarkdownDocCreationAttributes): Pr
     })
     return docRef.id
   } catch (e) {
+    console.error(e)
     throw new Error('Unable to create document')
   }
+}
+
+export const getMarkdownDocByIdAndUser = async (userEmail: string, docId: string): Promise<DocumentData> => {
+  const snapshot = await getDoc(doc(db, MARKDOWN_DOCS_COLLECTION_NAME, docId))
+
+  if (snapshot.exists()) {
+    const data = snapshot.data()
+    if (data.userId !== userEmail) throw new Error('Not found')
+    return {
+      ...data,
+      createdAt: new Date(data.createdAt.seconds * 1000).toISOString(),
+      lastEdit: new Date(data.lastEdit.seconds * 1000).toISOString()
+    }
+  } else throw new Error('Not found')
 }
