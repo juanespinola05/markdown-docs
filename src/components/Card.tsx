264002d6840cb808e499b143778350a5c1ada3bb
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-03-28 18:53:34 -0300

    remove shadow

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,7 +3,7 @@ import { FC, PropsWithChildren, ReactElement } from 'react'
 const Card: FC<PropsWithChildren<React.HTMLAttributes<HTMLDivElement>>> = ({ children, className, ...props }): ReactElement => {
   return (
     <div
-      className={`overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out ${className !== undefined ? className : ''}`}
+      className={`overflow-hidden rounded-lg bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out ${className !== undefined ? className : ''}`}
       {...props}
     >
       {children}
