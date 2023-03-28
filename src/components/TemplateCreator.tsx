5cc2b97b73f549d20437cdc0341fe3e0d9e9cb86
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-03-28 18:56:34 -0300

    add classname prop

--- src/components/TemplateCreator.tsx
+++ src/components/TemplateCreator.tsx
@@ -1,13 +1,13 @@
 import { FC, PropsWithChildren, ReactElement } from 'react'
 import Card from './Card'
 
-type IType = FC<PropsWithChildren>
+type IType = FC<PropsWithChildren & { className?: string }>
 
-const TemplateCreator: IType = ({ children, ...props }): ReactElement => {
+const TemplateCreator: IType = ({ children, className = '', ...props }): ReactElement => {
   return (
     <div className='inline-block' {...props}>
       <Card
-        className='w-48 h-60 max-w-xs'
+        className={`w-48 h-60 max-w-xs ${className}`}
       >
         {children}
       </Card>
