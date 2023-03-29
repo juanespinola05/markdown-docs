f8bfcc883fef59a35d8919fc3db79b9567c5c9a6
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-03-28 23:06:05 -0300

    fix prop typo and create getDocuments util

--- src/lib/firebase/actions/documents.ts
+++ src/lib/firebase/actions/documents.ts
@@ -1,15 +1,17 @@
-import { MarkdownDocCreationAttributes } from '@/types'
-import { addDoc, collection, doc, DocumentData, getDoc, Timestamp } from 'firebase/firestore'
+import { MarkdownDocCreationAttributes, UserData } from '@/types'
+import { serializeDocumentData } from '@/utils/firebase'
+import { addDoc, collection, doc, DocumentData, getDoc, getDocs, query, Timestamp, where } from 'firebase/firestore'
 import { db } from '../client'
 
 const MARKDOWN_DOCS_COLLECTION_NAME = 'markdown-documents'
+const markdownCollection = collection(db, MARKDOWN_DOCS_COLLECTION_NAME)
 
 export const createMarkdownDoc = async (data: MarkdownDocCreationAttributes): Promise<string> => {
   try {
-    const docRef = await addDoc(collection(db, MARKDOWN_DOCS_COLLECTION_NAME), {
+    const docRef = await addDoc(markdownCollection, {
       ...data,
       createdAt: Timestamp.fromDate(new Date()),
-      lastEdit: Timestamp.fromDate(new Date())
+      lastEdition: Timestamp.fromDate(new Date())
     })
     return docRef.id
   } catch (e) {
@@ -25,9 +27,20 @@ export const getMarkdownDocByIdAndUser = async (userEmail: string, docId: string
     const data = snapshot.data()
     if (data.userId !== userEmail) throw new Error('Not found')
     return {
-      ...data,
-      createdAt: new Date(data.createdAt.seconds * 1000).toISOString(),
-      lastEdit: new Date(data.lastEdit.seconds * 1000).toISOString()
+      ...serializeDocumentData(data)
     }
   } else throw new Error('Not found')
+}
+
+export const getMarkdownDoccumentsByUser = async (email: UserData['email']): Promise<DocumentData[]> => {
+  const q = query(markdownCollection, where('userId', '==', email))
+  const querySnapshot = await getDocs(q)
+
+  const documents: DocumentData[] = []
+  querySnapshot.forEach(doc => {
+    const data = serializeDocumentData(doc.data())
+    documents.push(data)
+  })
+
+  return documents
 }
