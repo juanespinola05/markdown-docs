8252b63c6ab8778de2cbdeb413ad18f39d6efa9c
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-03-28 23:28:58 -0300

    update icon

--- src/components/LatestDocument.tsx
+++ src/components/LatestDocument.tsx
@@ -3,6 +3,7 @@ import { MarkdownDocData } from '@/types'
 import { FC, ReactElement } from 'react'
 import Card from './Card'
 import DocumentCardOptions from './DocumentCardOptions'
+import DocumentIcon from '@/assets/document.svg'
 
 interface IProps extends Pick<MarkdownDocData, 'title' | 'lastEdition'> {
   docId: string
@@ -19,11 +20,11 @@ const LatestDocument: FC<IProps> = ({
       <Card className='h-80 w-full md:w-52 border-[1px] border-gray-200'>
         <div className='h-60' />
         <div className='h-20 border-[1px] px-2 py-4'>
-          <h4 className='text-slate-800'>{title}</h4>
+          <h4 className='text-slate-800 pl-1'>{title}</h4>
           <div className='flex items-center justify-between'>
-            <div className='flex gap-2'>
-              <div className='w-4 h-4 rounded-sm bg-orange-600' />
-              <p className='text-slate-600 text-xs'>Last edition {timeAgo}</p>
+            <div className='flex gap-2 items-center'>
+              <DocumentIcon className='w-6 fill-blue' />
+              <p className='text-slate-600 text-xs'>Edited {timeAgo}</p>
             </div>
             <DocumentCardOptions />
           </div>
