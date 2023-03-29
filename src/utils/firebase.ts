a98ef844b6be02b6704e51f0187ebf8c6008fde8
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-03-28 23:05:27 -0300

    feat: create doc data serielizer

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,5 +1,6 @@
 import { UserData } from '@/types'
 import { User } from 'firebase/auth'
+import { DocumentData, Timestamp } from 'firebase/firestore'
 
 export const mapUserFromFirebaseAuth = (user: User | null): UserData | null => {
   if (user === null) return null
@@ -9,4 +10,16 @@ export const mapUserFromFirebaseAuth = (user: User | null): UserData | null => {
     photoURL,
     email
   }
+}
+
+export const serializeDocumentData = (document: DocumentData): object => {
+  const entries = Object.entries(document)
+  const serialized = entries.map(([key, value]) => (
+    value instanceof Timestamp
+      ? [key, new Date(value.seconds * 1000).toISOString()]
+      : [key, value]
+  )
+  )
+
+  return Object.fromEntries(serialized)
 }
