1a6153f42a8a080df6ad82fbc57fa4f7bba57094
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-03-29 11:35:39 -0300

    move anchor to title

--- src/components/LatestDocument.tsx
+++ src/components/LatestDocument.tsx
@@ -15,21 +15,25 @@ const LatestDocument: FC<IProps> = ({
   docId
 }): ReactElement => {
   return (
-    <a href={`/compose/${docId}`} className='block' title={title}>
-      <Card className='h-80 w-full md:w-52 border-[1px] border-gray-200'>
-        <div className='h-60' />
-        <div className='h-20 border-[1px] px-2 py-4'>
-          <h4 className='text-slate-800 pl-1'>{title}</h4>
-          <div className='flex items-center justify-between'>
-            <div className='flex gap-2 items-center'>
-              <DocumentIcon className='w-6 fill-blue' />
-              <DocumentTimeAgo timestamp={lastEdition} />
-            </div>
-            <DocumentCardOptions />
+    <Card className='h-80 w-full md:w-52 border-[1px] border-gray-200'>
+      <div className='h-60' />
+      <div className='h-20 border-[1px] px-2 py-4'>
+        <a
+          href={`/compose/${docId}`}
+          title={title}
+          className='text-slate-800 pl-1 hover:underline'
+        >
+          {title}
+        </a>
+        <div className='flex items-center justify-between'>
+          <div className='flex gap-2 items-center'>
+            <DocumentIcon className='w-6 fill-blue' />
+            <DocumentTimeAgo timestamp={lastEdition} />
           </div>
+          <DocumentCardOptions />
         </div>
-      </Card>
-    </a>
+      </div>
+    </Card>
   )
 }
 
