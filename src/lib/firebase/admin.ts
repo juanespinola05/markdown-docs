2b780a57d926ebcddc6301c6dc4f097a5e922e52
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-03-29 19:11:32 -0300

    fix: admin connection

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,22 +1,16 @@
 import { credential } from 'firebase-admin'
 import { getFirestore } from 'firebase-admin/firestore'
 import { getAuth } from 'firebase-admin/auth'
-import { getApps, App, initializeApp, ServiceAccount } from 'firebase-admin/app'
+import { getApps, initializeApp, ServiceAccount } from 'firebase-admin/app'
 import { SERVICE_ACCOUNT } from '@/constants/environment'
 
 const apps = getApps()
-let app: App
+const app = (apps.length === 0)
+  ? initializeApp({
+    credential: credential.cert(SERVICE_ACCOUNT as ServiceAccount),
+    databaseURL: 'https://markdown-docs-20776-default-rtdb.firebaseio.com'
+  })
+  : apps[0]
 
-try {
-  app = (apps.length === 0)
-    ? initializeApp({
-      credential: credential.cert(SERVICE_ACCOUNT as ServiceAccount),
-      databaseURL: 'https://markdown-docs-20776-default-rtdb.firebaseio.com'
-    })
-    : apps[0]
-} catch (error) {}
-
-// @ts-expect-error
 export const adminAuth = getAuth(app)
-// @ts-expect-error
 export const adminFirestore = getFirestore(app)
