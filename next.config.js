9b19f06cb5fb308eebf657f6e9be7d505ad462ee
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-04-08 09:36:30 -0300

    turn off strict mode

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const removeImports = require('next-remove-imports')()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   webpack (config) {
     config.module.rules.push({
       test: /\.svg$/,
