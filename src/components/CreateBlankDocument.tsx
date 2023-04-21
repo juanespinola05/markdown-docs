968d00cac0036bfc11297bea8b7ce59c18955b58
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-04-21 18:25:43 -0300

    update styles

--- src/components/CreateBlankDocument.tsx
+++ src/components/CreateBlankDocument.tsx
@@ -4,6 +4,8 @@ import { FC, FormEventHandler, forwardRef, ReactElement, RefObject, useRef, useS
 import Popup from 'reactjs-popup'
 import Card from './Card'
 import Spinner from './Spinner'
+import Input from './Input'
+import DocumentIcon from '../assets/document.svg'
 
 const CreatorButton = forwardRef<RefObject<HTMLButtonElement>>((props, ref) => {
   return (
@@ -53,28 +55,28 @@ function CreatorForm ({ closePopup }: { closePopup: any }): ReactElement {
 
   return (
     <div
-      className='w-80 h-36 bg-white p-4 rounded-md flex flex-col justify-between'
+      className='p-4 bg-white dark:bg-slate-700 border border-gray-200 dark:border-slate-600 rounded-md shadow-lg'
     >
       {loading
-        ? <Spinner />
+        ? (
+          <div className='w-80 h-32'>
+            <Spinner />
+          </div>
+          )
         : (
-          <form onSubmit={handleDocumentCreation}>
-            <p className='text-lg text-slate-700  '>Create blank document</p>
-            <input
-              ref={inputRef}
-              placeholder='Your document name'
-              defaultValue='New Document'
-              className='rounded-md bg-white p-1 text-black w-full border-[1px] border-slate-400 h-9'
-              type='text'
-              minLength={1}
-              maxLength={32}
-            />
+          <form
+            onSubmit={handleDocumentCreation}
+            className='flex flex-col justify-between gap-2'
+          >
+            <p className='text-lg'>Create blank document</p>
+            <Input ref={inputRef} placeholder='Project name' defaultValue='New Document'>
+              <DocumentIcon className='w-5 mr-2 fill-slate-700' />
+            </Input>
             <div className='flex gap-2'>
               <button
                 type='submit'
-                className='p-1 px-2text-slate-500 items-center border-[1px] rounded-md border-slate-400 bg-slate-400 text-slate-900'
-              >
-                Create
+                className='h-9 px-4 rounded-md inline-flex flex-shrink-0 whitespace-nowrap items-center gap-2 transition-colors duration-150 ease-in-out leading-none border-1 cursor-pointer border-gray-200/60 bg-gray-200/60 text-gray-900 hover:bg-gray-200 hover:text-gray-900'
+              >Create
               </button>
               <button
                 type='button'
